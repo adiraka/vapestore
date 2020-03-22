c9fa719f5062e9af7ac01c884a609a65941265a4
Adi Raka Siwi <raka@wvi.com>
2020-03-23 01:17:39 +0700

    add role constant

---
+++ app/Util/Constant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Util;
+
+class Constant {
+
+    const USER_ROLE_SUPER_ADMIN = 'SUPER_ADMIN';
+    const USER_ROLE_ADMIN = 'ADMIN';
+    const USER_ROLE_CUSTOMER = 'CUSTOMER';
+
+}

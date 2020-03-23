122989362342be1382a794eb671476a8e5f32ca7
Adi Raka Siwi <raka@wvi.com>
2020-03-24 02:25:43 +0700

    add status constant

--- app/Util/Constant.php
+++ app/Util/Constant.php
@@ -8,4 +8,12 @@ class Constant {
     const USER_ROLE_ADMIN = 'ADMIN';
     const USER_ROLE_CUSTOMER = 'CUSTOMER';
 
+    const STATUS_ACTIVE = 'ACTIVE';
+    const STATUS_INACTIVE = 'INACTIVE';
+
+    const STATUS_LABELS = [
+    	self::STATUS_ACTIVE => 'Active',
+    	self::STATUS_INACTIVE => 'Inactive'
+    ];
+
 }

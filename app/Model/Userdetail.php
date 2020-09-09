ef66c68d3fabcdca6736af25929d7ed49b805108
Adi Raka Siwi <raka@wvi.co.id>
2020-09-09 08:16:52 +0700

    add table user detail

---
+++ app/Model/Userdetail.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetail extends Model
+{
+    protected $table = 'user_detail';
+
+    public function user() {
+    	return $this->belongsTo(User::class, 'user_id');
+    }
+}

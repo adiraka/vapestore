0dfe3c822c7353277f67d2704da070ce0e22851b
Adi Raka Siwi <raka@wvi.co.id>
2020-09-15 02:00:56 +0700

    adjust home page

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
  
 	@include('web.template.parts.slider-section', ['some' => 'data'])
-	@include('web.template.parts.advantage-section', ['some' => 'data'])
+	{{-- @include('web.template.parts.advantage-section', ['some' => 'data']) --}}
 	@include('web.template.parts.hot-section', ['some' => 'data'])
 	@include('web.template.parts.blog-section', ['some' => 'data'])
  
--- resources/views/web/template/parts/footer-section.blade.php
+++ resources/views/web/template/parts/footer-section.blade.php
@@ -1,62 +1,20 @@
 <div id="footer">
   <div class="container">
     <div class="row">
-      <div class="col-lg-3 col-md-6">
-        <h4 class="mb-3">Pages</h4>
-        <ul class="list-unstyled">
-          <li><a href="text.html">About us</a></li>
-          <li><a href="text.html">Terms and conditions</a></li>
-          <li><a href="faq.html">FAQ</a></li>
-          <li><a href="contact.html">Contact us</a></li>
-        </ul>
-        <hr>
-        <h4 class="mb-3">User section</h4>
-        <ul class="list-unstyled">
-          <li><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li>
-          <li><a href="register.html">Regiter</a></li>
-        </ul>
-      </div>
-      <!-- /.col-lg-3-->
-      <div class="col-lg-3 col-md-6">
-        <h4 class="mb-3">Top categories</h4>
-        <h5>Men</h5>
-        <ul class="list-unstyled">
-          <li><a href="category.html">T-shirts</a></li>
-          <li><a href="category.html">Shirts</a></li>
-          <li><a href="category.html">Accessories</a></li>
-        </ul>
-        <h5>Ladies</h5>
-        <ul class="list-unstyled">
-          <li><a href="category.html">T-shirts</a></li>
-          <li><a href="category.html">Skirts</a></li>
-          <li><a href="category.html">Pants</a></li>
-          <li><a href="category.html">Accessories</a></li>
-        </ul>
-      </div>
-      <!-- /.col-lg-3-->
+      <div class="col-lg-3 col-md-6"></div>
+      <div class="col-lg-3 col-md-6"></div>
       <div class="col-lg-3 col-md-6">
         <h4 class="mb-3">Where to find us</h4>
-        <p><strong>Obaju Ltd.</strong><br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p><a href="contact.html">Go to contact page</a>
+        <p><strong>Tuan Ngebul Vapehouse.</strong><br>Jl. Ratulangi No.25<br>Kp. Jao, Kec. Padang Bar<br>Kota Padang, Sumatera Barat<br>0823 9112 4838<br><strong>Indonesia</strong></p>
         <hr class="d-block d-md-none">
       </div>
-      <!-- /.col-lg-3-->
       <div class="col-lg-3 col-md-6">
-        <h4 class="mb-3">Get the news</h4>
-        <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        <form>
-          <div class="input-group">
-            <input type="text" class="form-control"><span class="input-group-append">
-              <button type="button" class="btn btn-outline-secondary">Subscribe!</button></span>
-          </div>
-          <!-- /input-group-->
-        </form>
-        <hr>
         <h4 class="mb-3">Stay in touch</h4>
-        <p class="social"><a href="#" class="facebook external"><i class="fa fa-facebook"></i></a><a href="#" class="twitter external"><i class="fa fa-twitter"></i></a><a href="#" class="instagram external"><i class="fa fa-instagram"></i></a><a href="#" class="gplus external"><i class="fa fa-google-plus"></i></a><a href="#" class="email external"><i class="fa fa-envelope"></i></a></p>
+        <p class="social">
+          <a href="https://api.whatsapp.com/send?phone=82391124838&text=Selamat%20Datang%20Di%20VapeStore" class="facebook external"><i class="fa fa-whatsapp"></i></a>
+          <a href="https://www.instagram.com/ex_vapestore/?hl=id" class="instagram external"><i class="fa fa-instagram"></i></a>
+        </p>
       </div>
-      <!-- /.col-lg-3-->
     </div>
-    <!-- /.row-->
   </div>
-  <!-- /.container-->
 </div>
